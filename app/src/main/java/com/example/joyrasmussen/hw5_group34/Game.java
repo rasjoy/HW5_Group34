348d5d4185260bed04c9b325ed667cd9fafdac6b
rasjoy <rasmussenjoy@gmail.com>
2017-02-15 14:39:04 -0500

    Added the game class

---
+++ app/src/main/java/com/example/joyrasmussen/hw5_group34/Game.java
@@ -0,0 +1,41 @@
+package com.example.joyrasmussen.hw5_group34;
+
+/**
+ * Created by joyrasmussen on 2/15/17.
+ */
+
+public class Game {
+    String id, title, platform, release;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+}
---
+++ app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetails_Activity.java
@@ -0,0 +1,8 @@
+package com.example.joyrasmussen.hw5_group34;
+
+/**
+ * Created by joyrasmussen on 2/15/17.
+ */
+
+public class GameDetails_Activity {
+}
