ccdcf518797e6f88ff799f83494960f05397ec6d
rasjoy <rasmussenjoy@gmail.com>
2017-02-15 14:45:08 -0500

    Added game details class

--- app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetail.java
+++ app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetail.java
@@ -4,5 +4,46 @@ package com.example.joyrasmussen.hw5_group34;
  * Created by joyrasmussen on 2/15/17.
  */
 
-public class GameDetail {
+public class GameDetail extends Game {
+        String imageUrl, videoUrl, overview, genre, publisher;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }
