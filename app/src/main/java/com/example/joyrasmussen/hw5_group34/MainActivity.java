0e051b33aa17fbcd333ad67356af240d410be053
rasjoy <rasmussenjoy@gmail.com>
2017-02-15 14:57:04 -0500

    Adding in listener for go button to start Game Details activity

--- app/src/main/java/com/example/joyrasmussen/hw5_group34/MainActivity.java
+++ app/src/main/java/com/example/joyrasmussen/hw5_group34/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.joyrasmussen.hw5_group34;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String GAME = "GAME";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void goClick(View v){
+        Intent intent = new Intent("com.example.joyrasmussen.hw5_group34.intent.action.Details");
 
-
-
-
+        startActivity(intent);
     }
 }
