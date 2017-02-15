3aac4a59b54daf4904c740ab93bc97ba1de55d1e
rasjoy <rasmussenjoy@gmail.com>
2017-02-15 14:48:37 -0500

    Forgot to create game activity as an activity

--- app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetailsActivity.java
+++ app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetailsActivity.java
@@ -2,21 +2,12 @@ package com.example.joyrasmussen.hw5_group34;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void goClick(View v){
-
-
-
-
+        setContentView(R.layout.activity_game_details);
     }
 }
--- app/src/main/java/com/example/joyrasmussen/hw5_group34/GameDetails_Activity.java
+++
@@ -1,8 +0,0 @@
-package com.example.joyrasmussen.hw5_group34;
-
-/**
- * Created by joyrasmussen on 2/15/17.
- */
-
-public class GameDetails_Activity {
-}
